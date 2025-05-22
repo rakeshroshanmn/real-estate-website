e6da143ec70df3aa2804dd2364bda0b14006fd0a
Rakesh Roshan M N <rakeshroshan@gmail.com>
2025-05-22 11:17:59 +1000

    port modification

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const routes = require('./routes/index');
+const path = require("path");
+const routes = require("./routes/index");
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
-app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "html");
+app.engine("html", require("ejs").renderFile);
 
-app.use('/', routes);
+app.use("/", routes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(3000, '0.0.0.0', () => {
-  console.log('Server running on http://0.0.0.0:3000');
+const PORT = 4411;
+app.listen(PORT, "localhost", () => {
+  console.log(`Server running at: http://localhost:${PORT}`);
 });
